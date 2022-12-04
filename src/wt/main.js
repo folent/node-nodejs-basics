6237d9db49894633c6d0c55d7c1abd4d27c84852
Vadim <folent1@yandex.ru>
2022-12-04 22:20:10 +0300

    feat: add solutions for Workers tasks

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,37 @@
+import { Worker, isMainThread } from 'node:worker_threads';
+import { cpus } from 'node:os'
+
 const performCalculations = async () => {
-    // Write your code here
+    const workerPath = new URL('worker.js', import.meta.url);
+
+    if (isMainThread) {
+        const cpuCount = cpus().length;
+        let number = 10;
+
+        const workers = Array(cpuCount)
+        .fill()
+        .map(_ => {
+            return new Promise((resolve) => {
+                const worker = new Worker(workerPath, { workerData: number++ });
+
+                worker.on("message", data => {
+                    resolve({
+                        status: 'resolved',
+                        data
+                    })
+                });
+                worker.on("error", _ => {
+                    resolve({
+                        status: 'error',
+                        data: null
+                    })
+                });
+              });
+        })
+
+        const results = await Promise.all(workers);
+        console.log(results);
+    }
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,20 @@
+import { parentPort, workerData, isMainThread } from 'node:worker_threads';
+
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+    if(!isMainThread) {
+
+        if (!Number.isInteger(workerData)) {
+            throw new Error("workerData must be number");
+        }
+
+        parentPort.postMessage(
+            nthFibonacci(workerData)
+        );
+    }
+
 };
 
 sendResult();
