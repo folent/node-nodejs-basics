b41dd80d387d1b28beb6fa4be316f6ff5fd21f9e
Vadim <folent1@yandex.ru>
2022-12-04 22:26:22 +0300

    feat: add solutions for Zip tasks

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,27 @@
+import { createReadStream, createWriteStream, } from 'node:fs';
+import { createUnzip } from 'node:zlib';
+import { pipeline } from 'node:stream';
+
 const decompress = async () => {
-    // Write your code here 
+    const fileName = './files/fileToCompress.txt';
+    const archiveName = './files/archive.txt.gz';
+    const filePath = new URL(fileName, import.meta.url);
+    const archivePath = new URL(archiveName, import.meta.url);
+
+    const input = createReadStream(archivePath);
+    const output = createWriteStream(filePath, 'utf-8');
+    const unZip = createUnzip();
+
+    pipeline(
+        input,
+        unZip,
+        output,
+        err => {
+            if (err) {
+                console.error(err);
+            }
+        }
+    );
 };
 
 await decompress();
