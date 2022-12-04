d4b9a9fe435fca79817872cca7f542313bc96c1c
Vadim <folent1@yandex.ru>
2022-12-04 21:46:10 +0300

    feat: add solutions for cli tasks

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,17 @@
 const parseEnv = () => {
-    // Write your code here 
+    const filteredKeys = Object.keys(process.env).filter(el => el.includes('RSS_'));
+    let result = '';
+
+    for (let i = 0; i < filteredKeys.length; i++) {
+        const key = filteredKeys[i];
+
+        if(key === filteredKeys[filteredKeys.length - 1]) {
+            result += `${key}=${process.env[key]}`;
+        } else {
+            result += `${key}=${process.env[key]}; `;
+        }
+    }
+    console.log(result);
 };
 
 parseEnv();
