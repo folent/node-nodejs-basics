77e3e8c285e8f5745f5d48d3cb39cb3b8231dae7
Vadim <folent1@yandex.ru>
2022-12-04 22:16:27 +0300

    feat: add solutions for Streams tasks

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,22 @@
+import { Transform } from 'node:stream'
+
 const transform = async () => {
-    // Write your code here 
+    try {
+        const { stdin, stdout, exit } = process;
+
+        const transformAction = new Transform({
+            transform(chunk) {
+                const transformChunk = chunk.toString().split('').reverse().join('');
+                this.push(transformChunk + '\n')
+                exit()
+            }
+        })
+
+        stdin.pipe(transformAction).pipe(stdout)
+
+    } catch (e) {
+        console.error(e)
+    }
 };
 
 await transform();
