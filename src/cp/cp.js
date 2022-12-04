ef94d9208c82f57f7757e9bb1c2029803786c7f2
Vadim <folent1@yandex.ru>
2022-12-04 22:03:51 +0300

    feat: add solutions for Child Processes tasks

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,13 @@
+import { fork } from 'node:child_process';
+import { pathToFileURL } from 'node:url'
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const workerPath = pathToFileURL('./src/cp/files/script.js');
+
+    fork(workerPath, args);
+    process.stdin.on('data', chunk => {
+        process.stdout.write(`Received from child process: ${chunk.toString()}\n`)
+    });
 };
 
 // Put your arguments in function call to test this functionality
